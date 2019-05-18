940c86af8d9c91d4c19d6d6f8788ff3755f1bfea
Ridwan <Olarid7852@gmail.com>
2019-05-18 14:33:48 +0100

    Update removed some fields from patient model

--- medsoft/patient/models.py
+++ medsoft/patient/models.py
@@ -6,9 +6,6 @@ from django.utils.translation import gettext as _
 
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(_("name"), max_length=50)
-    phone_no = models.CharField(_("phone_no"), max_length=50)
-    email = models.EmailField(_("email"), max_length=254)
     address = models.CharField(_("address"), max_length=255)
     occupation = models.CharField(_("occupation"), max_length=255)
     dob = models.DateField()
